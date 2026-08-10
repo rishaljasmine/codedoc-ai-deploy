25892bb74b95faa1662d316d83af44d0284acbf7
tatvaverseinofficial-gif <tatvaverse.in.official@gmail.com>
2026-08-10 12:55:10 +0530

    Fix frontend documentation build

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -69,9 +69,8 @@
 
 //   return data;
 // }
+const API_URL = "https://codedoc-ai-deploy.onrender.com"||import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
-
-const API_URL ="https://codedoc-ai-deploy.onrender.com" ;
 
 export async function generateDocumentation(code) {
   const response = await fetch(`${API_URL}/generate`, {
